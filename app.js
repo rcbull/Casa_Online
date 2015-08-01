cc59341cfe953edf6d191d8e0722ad41e8689226
Jorge Fernando Damasio Leite <jorgefernandodamasio@gmail.com>
2015-08-01 00:07:00 -0300

    estruturacao de modulos

--- app.js
+++ app.js
@@ -3,17 +3,28 @@ var app  = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
+
+
+//importando o módulo responsável por efetuar a comunicação do node com o arduino
 var five = require("johnny-five");
+
+//instanciando uma placa que deve estar conectada ao servidor
 var board = new five.Board();
 
-var five = require("johnny-five");
-// var board = new five.Board();
+
+//importando o módulo que será responsável pela conexão com a base e pelos modelos de dados
+var database= require("./modules/dataBaseConnect.js");
+
+database.erro("testando a função criada no módulo");
+
+
 
 app.use("/", express.static(path.join(__dirname, '/files/pages')));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/files/pages/index.html');
 });
+
 
 io.on('connection', function(socket ){
 
@@ -25,11 +36,16 @@ io.on('connection', function(socket ){
 
 
 
+//instanciando o módulo reponsável pela comunicação com o mongodb
 var Mongoose = require('Mongoose');
 
 var db = Mongoose.connection;
+db.on('error', function()
+{
+	console.log('erro ao conectar na base de dados');
 
-db.on('error', console.error);
+});;
+
 db.once('open', function() {
   console.log('Conectado ao MongoDB.')
   // Vamos adicionar nossos Esquemas, Modelos e consultas aqui
---
+++ modules/dataBaseConnect.js
@@ -0,0 +1,8 @@
+
+//função que demonstra o funcionamento dos módulos no node js
+exports.erro = function(msg)
+{
+
+	console.log(msg);
+
+}
