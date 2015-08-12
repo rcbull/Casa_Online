824034bda1a73c135487e1eb749efa91ac34c6b3
Jorge Fernando Damasio Leite <jorgefernandodamasio@gmail.com>
2015-08-11 23:23:30 -0300

    configuracao MVC

--- app.js
+++ app.js
@@ -1,3 +1,55 @@
+var express = require('express'),
+	app  = express(),
+	cons = require('consolidate');
+
+
+//importando o módulo que será responsável pela conexão com a base e pelos modelos de dados
+var database= require("./modules/dataBaseConnect.js");
+//database.erro("testando a função criada no módulo");
+
+database.ConectarBancoDeDados();
+
+
+
+
+
+app.engine('html',cons.swig);
+app.set('view engine','html');
+app.set('views',	__dirname +	"/files/pages");
+
+
+app.get('/',function(req,res){
+
+	res.render('index',{nome:'Fernando',linguagem:'java'});
+
+
+});
+
+app.get('/node',function(req,res){
+
+	res.render('index',{nome:'Fernando',linguagem:'node.js'});
+
+
+});
+
+
+app.get('*',function(req,res){
+
+	res.send("Page not found",404);
+
+});
+
+app.listen(8080);
+console.log("Express server running on port 8080");
+
+
+
+
+
+
+
+/* backup
+
 var express = require('express');
 var app  = express();
 var http = require('http').Server(app);
@@ -39,6 +91,7 @@ io.on('connection', function(socket ){
 
 
 
+*/
 
 
 
@@ -49,6 +102,14 @@ io.on('connection', function(socket ){
 
 
 
-http.listen(9000, function(){
-  console.log('listening on *:9000');
-});
+
+
+
+
+
+
+
+
+
+
+
